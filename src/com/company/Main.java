929f3a65aae2cdf903f307e8036cc77ab9889e40
iiropa <iiropaajanen2004@gmail.com>
2021-09-01 10:26:28 +0300

    Amazing, ground-breaking, revolutionary code

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int weight = 4;
+        int price = 0;
+
+        if (weight <= 2) {
+            price = 3;
+        }
+        if (weight > 2 && weight <= 5) {
+            price = 3 + 2 * (weight - 2);
+        }
+        if (weight > 5) {
+            price = 9 + 3 * (weight - 5);
+        }
+        System.out.println("The price is " + price);
+
+    }
+
+
+}
